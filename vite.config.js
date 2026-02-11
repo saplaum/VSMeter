8e4130cb53b4bcd12e008a79ee800b13e702d922
Sascha Plaum <sascha.plaum@mercedes-benz.com>
2026-02-11 22:59:25 +0100

    Fix base path: add /SAPLAUM/ prefix for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/VSMeter/',
+  base: '/SAPLAUM/VSMeter/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
